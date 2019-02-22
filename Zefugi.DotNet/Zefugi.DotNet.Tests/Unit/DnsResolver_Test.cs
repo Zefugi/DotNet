8cde0885938371ac23baf5d69d6f93ae86091d18
Zefugi <trowi@live.dk>
2019-02-22 12:16:42 +0100

    Implemented ResolveOrParse and acompanying test.

---
+++ Zefugi.DotNet/Zefugi.DotNet.Tests/Unit/DnsResolver_Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Zefugi.DotNet.Net;
+
+namespace Zefugi.DotNet.Tests.Unit
+{
+    [TestClass]
+    public class DnsResolver_Test
+    {
+        [TestMethod]
+        public void ResolveLocalhost()
+        {
+            Assert.IsTrue(DnsResolver.ResolveOrParse("localhost", 1337, AddressFamily.InterNetwork, out IPEndPoint ep));
+            Assert.AreEqual(AddressFamily.InterNetwork, ep.AddressFamily);
+            Assert.AreEqual(IPAddress.Parse("127.0.0.1"), ep.Address);
+            Assert.AreEqual(1337, ep.Port);
+        }
+    }
+}
---
+++ Zefugi.DotNet/Zefugi.DotNet/Net/DnsResolver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Zefugi.DotNet.Net
+{
+    public static class DnsResolver
+    {
+        public static bool ResolveOrParse(
+            string hostNameOrAddress, int port, AddressFamily addressFamily,
+            out IPEndPoint endPoint)
+        {
+            IPAddress ip = null;
+            IPAddress[] list;
+
+            // Parse
+            if (IPAddress.TryParse(hostNameOrAddress, out ip))
+            {
+                endPoint = new IPEndPoint(ip, port);
+                return true;
+            }
+
+            // Resolve
+            try { list = Dns.GetHostEntry(hostNameOrAddress).AddressList; }
+            catch
+            {
+                endPoint = null;
+                return false;
+            }
+            foreach (IPAddress entry in list)
+            {
+                if (entry.AddressFamily == addressFamily)
+                {
+                    endPoint = new IPEndPoint(entry, port);
+                    return true;
+                }
+            }
+            endPoint = null;
+            return false;
+        }
+    }
+}
