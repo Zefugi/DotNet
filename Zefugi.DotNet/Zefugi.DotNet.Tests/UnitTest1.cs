f8776c1b0024d3af5beaf5bd144d0ce1270e51ba
Zefugi <trowi@live.dk>
2019-02-22 09:20:15 +0100

    Added a unit test for PooledObject*.

--- Zefugi.DotNet/Zefugi.DotNet.Tests/UnitTest1.cs
+++ Zefugi.DotNet/Zefugi.DotNet.Tests/UnitTest1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Zefugi.DotNet.Optimization;
 
 namespace Zefugi.DotNet.Tests
 {
@@ -9,6 +12,36 @@ namespace Zefugi.DotNet.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            new MyTest().Run();
+        }
+    }
+
+    class MyObj : PooledObjectBase<MyObj>
+    {
+        static int _idSpinner = 0;
+        public int ID { get; internal set; }
+        public MyObj()
+        {
+            if (++_idSpinner == int.MaxValue)
+                _idSpinner = 1;
+            ID = _idSpinner;
+        }
+    }
+
+    class MyTest
+    {
+        public void Run()
+        {
+            var a = MyObj.CreateOrReuse();
+            var aid = a.ID;
+            var b = MyObj.CreateOrReuse();
+            var bid = b.ID;
+            a.Recycle();
+            var c = MyObj.CreateOrReuse();
+            var cid = c.ID;
+            Assert.AreEqual(1, aid);
+            Assert.AreEqual(2, bid);
+            Assert.AreEqual(1, aid);
         }
     }
 }
